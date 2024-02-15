0ea15b1d72cb183c72855b750a1e45a2e3730fd8
georgie-dev <mangergeorgepraise@gmail.com>
2024-02-15 01:12:56 +0100

    Coin data

--- oxchain/urls.py
+++ oxchain/urls.py
@@ -4,6 +4,6 @@ from .views import register_user, user_login, CryptoDataSet
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
-    path('data/', CryptoDataSet.as_view(), name='data')
+    path('data/', CryptoDataSet.as_view({'get': 'list'}), name='data')
     # path('logout/', user_logout, name='logout'),
 ]
