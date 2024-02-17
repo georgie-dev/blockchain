5cbe110aafd3911af3e619520ce6f4153543473a
georgie-dev <mangergeorgepraise@gmail.com>
2024-02-17 18:02:30 +0100

    filter

--- oxchain/urls.py
+++ oxchain/urls.py
@@ -4,7 +4,8 @@ from .views import register_user, user_login, CryptoDataSet, Users
 urlpatterns = [
     path('register/', register_user, name='register'),
     path('login/', user_login, name='login'),
-    path('data/', CryptoDataSet.as_view({'get': 'list', 'delete': 'destroy', 'put': 'update'}), name='data'),
+    path('data/', CryptoDataSet.as_view({'get': 'list'}), name='data'),
+    path('data/<int:pk>/', CryptoDataSet.as_view({'delete': 'destroy', 'put': 'update'}), name='data-detail'),
     path('users/', Users.as_view({'get': 'list'}), name='user')
     # path('logout/', user_logout, name='logout'),
 ]
